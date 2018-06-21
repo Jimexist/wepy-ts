190479e612b474ac00ce3b32e8a61375511a9cc6
Jiayu Liu <jiayu.liu@airbnb.com>
2018-06-21 23:24:13 +0800

    use typescript

--- wepy.config.js
+++ wepy.config.js
@@ -1,9 +1,25 @@
+const path = require("path");
+const prod = process.env.NODE_ENV === "production";
+
 module.exports = {
+  wpyExt: ".wpy",
+  cliLogs: !prod,
+  resolve: {
+    modules: ["node_modules"]
+  },
   compilers: {
+    less: {
+      compress: prod
+    },
     typescript: {
       compilerOptions: {
-        module: system
+        module: "system",
+        noImplicitAny: true,
+        removeComments: true,
+        preserveConstEnums: true,
+        sourceMap: true
       }
     }
-  }
+  },
+  plugins: {}
 };
